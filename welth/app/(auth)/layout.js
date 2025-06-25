1b7bf0d3b700ebd18ae89f83780d51fc4204023d
Tushar <tg16072005@gmail.com>
2025-06-25 20:06:44 +0530

    Preparing for deployment

---
+++ welth/app/(auth)/layout.js
@@ -0,0 +1,7 @@
+import React from 'react'
+import './Authlayout.css';
+const AuthLayout = ({ children }) => {
+    return <div className="auth-layout">{children}</div>
+}
+
+export default AuthLayout;
